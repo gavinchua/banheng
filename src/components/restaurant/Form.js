0948f0556f4be74198d650d0a444588675fea666
Gavin Chua <me@gavinchua.com>
2018-06-08 10:58:43 +0800

    minor fix and update packages

--- src/components/restaurant/Form.js
+++ src/components/restaurant/Form.js
@@ -124,7 +124,7 @@ export default class RestaurantForm extends Component {
             handleBlur,
             setFieldValue
           }) => (
-            <form onSubmit={handleSubmit}>
+            <form method="post" onSubmit={handleSubmit}>
               <p>
                 This form is WIP. Its not going anywhere yet. formData is
                 generated.
